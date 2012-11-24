5d351743159c7655e2574085939217f795533984
Adam Green <adamgr@foo.bar>
2012-11-23 22:13:13 -0800

    Update version information.

--- mri/version.h
+++ mri/version.h
@@ -6,7 +6,7 @@
 
 #define MRI_VERSION_MAJOR       0
 #define MRI_VERSION_MINOR       4
-#define MRI_VERSION_BUILD       20121114
+#define MRI_VERSION_BUILD       20121123
 #define MRI_VERSION_SUBBUILD    1
 
 #define MRI_STR(X) MRI_STR2(X)
