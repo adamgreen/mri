7f48c28d108a355fe47bea1f046a96929cf7b46e
Adam Green <adamgr@foo.bar>
2014-04-08 17:55:57 -0700

    Update copyright year for files recently touched

--- mri/buffer.c
+++ mri/buffer.c
@@ -1,4 +1,4 @@
-/* Copyright 2012 Adam Green (http://mbed.org/users/AdamGreen/)
+/* Copyright 2014 Adam Green (http://mbed.org/users/AdamGreen/)
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published
