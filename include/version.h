6e4387e259fe02c2562ed1ed6bdd97d0d5fbd710
Adam Green <adamgr@foo.bar>
2014-05-12 20:32:45 -0700

    Create branch for version 0.6
    This is to be used for my latest gcc4mbed update.  It contains the
    updates that were made while integrating the MRI core into pinkySim.

--- include/version.h
+++ include/version.h
@@ -6,8 +6,8 @@
 
 #define MRI_VERSION_MAJOR       0
 #define MRI_VERSION_MINOR       6
-#define MRI_VERSION_BUILD       20140303
-#define MRI_VERSION_SUBBUILD    0
+#define MRI_VERSION_BUILD       20140512
+#define MRI_VERSION_SUBBUILD    1
 
 #define MRI_STR(X) MRI_STR2(X)
 #define MRI_STR2(X) #X
