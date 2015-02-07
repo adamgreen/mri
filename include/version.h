a54ef05ec0d79165d71dbc9ae08286b363d07883
Adam Green <adamgr@foo.bar>
2015-02-07 14:42:56 -0800

    Update version information

--- include/version.h
+++ include/version.h
@@ -2,12 +2,12 @@
 
 #ifndef MRI_VERSION_STRING
 
-#define MRI_BRANCH "https://github.com/adamgreen/mri"
+#define MRI_BRANCH "https://github.com/adamgreen/mri/tree/version_0.7"
 
 #define MRI_VERSION_MAJOR       0
 #define MRI_VERSION_MINOR       7
-#define MRI_VERSION_BUILD       20150201
-#define MRI_VERSION_SUBBUILD    0
+#define MRI_VERSION_BUILD       20150207
+#define MRI_VERSION_SUBBUILD    1
 
 #define MRI_STR(X) MRI_STR2(X)
 #define MRI_STR2(X) #X
