675f48aa8ce06d92abb9791ad4f9f4a81fe46192
Adam Green <adamgr@foo.bar>
2015-12-07 17:38:41 -0800

    Updating master branch version to 0.9

--- include/version.h
+++ include/version.h
@@ -5,8 +5,8 @@
 #define MRI_BRANCH "https://github.com/adamgreen/mri"
 
 #define MRI_VERSION_MAJOR       0
-#define MRI_VERSION_MINOR       8
-#define MRI_VERSION_BUILD       20150207
+#define MRI_VERSION_MINOR       9
+#define MRI_VERSION_BUILD       20151207
 #define MRI_VERSION_SUBBUILD    0
 
 #define MRI_STR(X) MRI_STR2(X)
