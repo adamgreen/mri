5ac2273c10a0dc36aab295d72844e8291e7c9cb3
Adam Green <adamgreen@users.noreply.github.com>
2022-07-22 21:07:45 -0700

    Bump version build #

--- core/version.h
+++ core/version.h
@@ -6,7 +6,7 @@
 
 #define MRI_VERSION_MAJOR       1
 #define MRI_VERSION_MINOR       5
-#define MRI_VERSION_BUILD       20220720
+#define MRI_VERSION_BUILD       20220722
 #define MRI_VERSION_SUBBUILD    0
 
 #define MRI_STR(X) MRI_STR2(X)
