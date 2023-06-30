f803ff6d5eb6ba9e1debc100265848c83f17564e
ALTracer <tolstov_den@mail.ru>
2023-06-30 18:45:20 +0300

    boards: Initial support for WeAct Studio MiniF4 STM32F411CE
    * Implement platform code like for STM32F429I-Discovery,
      substituting differences due to board layout etc.
    * The entire MRI footprint is 15k-20k Flash + 1.5k SRAM,
      and it is quite useful when running simple blinkies
      without a dedicated external hardware debugger.
    * STM32CubeFW_F4 is still required for USART & CMSIS.

---
+++ boards/stm32f411-blackpill/stm32f411-blackpill.c
@@ -0,0 +1,39 @@
+/* Copyright 2015 Adam Green     (https://github.com/adamgreen/)
+   Copyright 2015 Chang,Jia-Rung (https://github.com/JaredCJR)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+/* Routines which expose STM32F411 Blackpill specific functionality to the mri debugger. */
+#include <string.h>
+#include <core/platforms.h>
+#include <core/try_catch.h>
+#include <architectures/armv7-m/debug_cm3.h>
+#include <devices/stm32f411xx/stm32f411xx_init.h>
+
+
+void Platform_Init(Token* pParameterTokens)
+{
+    mriStm32f411xx_Init(pParameterTokens);
+}
+
+
+const uint8_t* Platform_GetUid(void)
+{
+    return NULL;
+}
+
+
+uint32_t Platform_GetUidSize(void)
+{
+    return 0;
+}
